a37c77c3de7f49927ec1608043b712fd7a98571f
djodre <jodre.diangienda@gmail.com>
2022-04-27 14:12:20 +0200

    Ajout de la progressBar

--- app/src/main/java/com/example/td_20220426_myapplication_webservices/MainActivity.kt
+++ app/src/main/java/com/example/td_20220426_myapplication_webservices/MainActivity.kt
@@ -3,27 +3,58 @@ package com.example.td_20220426_myapplication_webservices
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import com.example.td_20220426_myapplication_webservices.databinding.ActivityMainBinding
+import com.example.td_20220426_myapplication_webservices.model.Article
 import com.example.td_20220426_myapplication_webservices.webservice.RetrofitInstance
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.Dispatcher
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.allArticles.setOnClickListener{
+            CoroutineScope(Dispatchers.Main).launch {
+                var bool = true
+                binding.progressBar.visibility = VISIBLE
+                withContext(Dispatchers.IO) {
+                    Thread.sleep(3000)
+                    val articles = RetrofitInstance.instance.getArticle()
+                    Log.d("articles", articles.toString())
+                        bool = false
+                }
+                if (!bool) binding.progressBar.visibility = GONE
+            }
+        }
+
+        binding.insertArticle.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                val articles = RetrofitInstance.instance.getArticles()
-                Log.d("articles", articles.toString())
+                val article = RetrofitInstance.instance.createArticle(Article(0, 1, "Retrofit", "Insert"))
+                Log.d("articles", article.toString())
+            }
+        }
+
+        binding.updateArticle.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val article = RetrofitInstance.instance.updateArticle(23, Article(23,1, "Retrofit", "Update1"))
+                Log.d("articles", article.toString())
+            }
+        }
+
+        binding.deleteArticle.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                RetrofitInstance.instance.deleteArticle(23)
+                Log.d("articles", "Deleted article")
             }
         }
     }
--- app/src/main/java/com/example/td_20220426_myapplication_webservices/webservice/DaoArticle.kt
+++ app/src/main/java/com/example/td_20220426_myapplication_webservices/webservice/DaoArticle.kt
@@ -1,15 +1,20 @@
 package com.example.td_20220426_myapplication_webservices.webservice
 
 import com.example.td_20220426_myapplication_webservices.model.Article
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface DaoArticle {
     @GET("articles")
-    suspend fun getArticles(): List<Article>
+    suspend fun getArticle(): List<Article>
 
-    @POST("articles/")
+    @POST("articles")
     suspend fun createArticle(@Body article : Article): Article
+
+    @PUT("article/{id}")
+    suspend fun updateArticle(@Path("id") articleId: Int, @Body article: Article): Article
+
+
+    @DELETE("article/{id}")
+    suspend fun deleteArticle(@Path("id") articleId: Int)
 
 }
